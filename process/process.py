d77f53e3a4293022aee92a7e3a1e75c317136db7
Luiz Andrade <lcoandrade@gmail.com>
2024-09-30 23:58:52 -0500

    Removing Hurst R/S with fixed subserie size calculation

--- process/process.py
+++ process/process.py
@@ -409,49 +409,7 @@ class ProcessStation():
             path=self.config["hurst_chan_plot_path"],
         )
 
-        # hurst_dict["local"] = self.hurst()
-
         return hurst_dict
-
-    def hurst(self):
-        ts = list(self.data[self.variable])
-        N = len(ts)
-        if N < 20:
-            raise ValueError(
-                "Time series is too short! input series ought to have at least 20 samples!"
-            )
-
-        max_k = int(np.floor(N / 2))
-        R_S_dict = []
-        for k in range(10, max_k + 1):
-            R, S = 0, 0
-            # split ts into subsets
-            subset_list = [ts[i : i + k] for i in range(0, N, k)]
-            if np.mod(N, k) > 0:
-                subset_list.pop()
-                # tail = subset_list.pop()
-                # subset_list[-1].extend(tail)
-            # calc mean of every subset
-            mean_list = [np.mean(x) for x in subset_list]
-            for i in range(len(subset_list)):
-                cumsum_list = pd.Series(subset_list[i] - mean_list[i]).cumsum()
-                R += max(cumsum_list) - min(cumsum_list)
-                S += np.std(subset_list[i])
-            R_S_dict.append(
-                {"R": R / len(subset_list), "S": S / len(subset_list), "n": k}
-            )
-
-        log_R_S = []
-        log_n = []
-        for i in range(len(R_S_dict)):
-            R_S = (R_S_dict[i]["R"] + np.spacing(1)) / (
-                R_S_dict[i]["S"] + np.spacing(1)
-            )
-            log_R_S.append(np.log(R_S))
-            log_n.append(np.log(R_S_dict[i]["n"]))
-
-        Hurst_exponent = np.polyfit(log_n, log_R_S, 1)[0]
-        return Hurst_exponent
 
     def hurst_ernest_chan(self, n_lags):
         """Returns the Hurst Exponent of the time series vector ts"""
